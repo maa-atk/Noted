8a492f8e33cc9aabe7ef2c57cfcf0a1120744347
maa-atk <maa.abhitk@gmail.com>
2021-03-16 17:39:04 +0530

    new

--- website/auth.py
+++ website/auth.py
@@ -38,35 +38,30 @@ def logout():
     return redirect(url_for('auth.login'))
 
 
-@auth.route('/sign_up', methods=["GET", "POST"])
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    if request.method == "POST":
-        email = request.form.get("email")
-        first_name = request.form.get("firstName")
-        password1 = request.form.get("password1")
-        password2 = request.form.get("password2")
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
 
-        if len(email) < 10:
-            flash('Email must be greater than 10 char',
-                  category="error")  # for flashing cards
-            pass
-        if password1 != password2:
-            flash('Passwords Do not match',
-                  category="error")  # for flashing cards
-            pass
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 2:
+            flash('Password must be at least 2 characters.', category='error')
         else:
-            # add user function for adding user
-            user = User.query.filter_by(email=email).first()
-            if user:
-                flash("Email already exists", category="success")
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
+                password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
 
-            else:
-                new_user = User(email=email, first_name=first_name, password=generate_password_hash(
-                    password1, method='sha256'))
-                db.session.add(new_user)
-                db.session.commit()
-                login_user(user, remember=True)
-                flash("Account created", category="success")
-            # safer route
-                return redirect(url_for('views.home'))
     return render_template("sign_up.html", user=current_user)
